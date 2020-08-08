54abadf6158e1b4dff2f4db2efd8e046f1d1cd02
Sanjay Sahu Bandla <54032677+Sanjay-Sahu-Bandla@users.noreply.github.com>
2020-08-08 13:03:00 +0530

    Able to toggle show more projects #portfolio

--- assets/dist/control.js
+++ assets/dist/control.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
-	$('li.nav-item').eq(0).addClass('active');
+  $('li.nav-item').eq(0).addClass('active');
 });
 
 $('li.nav-item').click(function() {
-	$('li.nav-item').removeClass('active');	
-	$(this).addClass('active');
+  $('li.nav-item').removeClass('active');	
+  $(this).addClass('active');
 });
 
 //change active class of navbar
 
 var sections = $('section')
-  , nav = $('nav')
-  , nav_height = nav.outerHeight();
+, nav = $('nav')
+, nav_height = nav.outerHeight();
 
 $(window).on('scroll', function () {
   var cur_pos = $(this).scrollTop();
   
   sections.each(function() {
     var top = $(this).offset().top - nav_height,
-        bottom = top + $(this).outerHeight();
+    bottom = top + $(this).outerHeight();
     
     if (cur_pos >= top && cur_pos <= bottom) {
       nav.find('li').removeClass('active');
@@ -32,7 +32,7 @@ $(window).on('scroll', function () {
 
 nav.find('a').on('click', function () {
   var $el = $(this)
-    , id = $el.attr('href');
+  , id = $el.attr('href');
   
   $('html, body').animate({
     scrollTop: $(id).offset().top - nav_height + 100
@@ -45,30 +45,30 @@ nav.find('a').on('click', function () {
 
 var zero = 0;
 $(document).ready(function() {
-	$(window).on('scroll', function() {
-		$('nav').toggleClass('hide', $(window).scrollTop() > zero);
-		zero = $(window).scrollTop();
-	});
+  $(window).on('scroll', function() {
+    $('nav').toggleClass('hide', $(window).scrollTop() > zero);
+    zero = $(window).scrollTop();
+  });
 });
 
 $(document).ready(function(){
   // Add smooth scrolling to all links
   $("a").on('click', function(event) {
-
+    
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       // Prevent default anchor click behavior
       event.preventDefault();
-
+      
       // Store hash
       var hash = this.hash;
-
+      
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 800, function(){
-
+        
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
@@ -79,13 +79,13 @@ $(document).ready(function(){
 //circular progress
 
 $(function() {
-
+  
   $("#prof-skills .progress").each(function() {
-
+    
     var value = $(this).attr('data-value');
     var left = $(this).find('.progress-left .progress-bar');
     var right = $(this).find('.progress-right .progress-bar');
-
+    
     if (value > 0) {
       if (value <= 50) {
         right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)')
@@ -94,13 +94,41 @@ $(function() {
         left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)')
       }
     }
-
+    
   })
-
+  
   function percentageToDegrees(percentage) {
-
+    
     return percentage / 100 * 360
-
+    
   }
+  
+});
 
+// displaying 4 recent projects
+$(document).ready(function () {
+
+  $('#showLess button').hide();
+  
+  size_li = $("#portfolio div.row").length;
+  x=4;
+
+  $('#portfolio div.row').hide();
+  $('#portfolio div.row:lt('+x+')').show();
+
+  $('#loadMore button').click(function () {
+    x = x+5;
+    $('#portfolio div.row:lt('+x+')').show();
+
+    $('#loadMore button').hide();
+    $('#showLess button').show();
+  });
+
+  $('#showLess button').click(function () {
+    x = x-5;
+    
+    $('#showLess button').hide();
+    $('#loadMore button').show();
+    $('#portfolio div.row').not(':lt('+x+')').hide();
+  });
 });
